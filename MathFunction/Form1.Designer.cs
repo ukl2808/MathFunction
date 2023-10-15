9f71b059f6b8b3fff615dbf7571225d6854c77d5
Admin <Admin@DESKTOP-047GBOQ>
2023-10-15 11:56:50 +0300

    Добавьте файлы проекта.

---
+++ MathFunction/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace MathFunction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxGraph = new System.Windows.Forms.PictureBox();
+            this.textBoxFunction = new System.Windows.Forms.TextBox();
+            this.buttonPlot = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxGraph
+            // 
+            this.pictureBoxGraph.Location = new System.Drawing.Point(126, 35);
+            this.pictureBoxGraph.Name = "pictureBoxGraph";
+            this.pictureBoxGraph.Size = new System.Drawing.Size(512, 241);
+            this.pictureBoxGraph.TabIndex = 0;
+            this.pictureBoxGraph.TabStop = false;
+            // 
+            // textBoxFunction
+            // 
+            this.textBoxFunction.Location = new System.Drawing.Point(181, 310);
+            this.textBoxFunction.Name = "textBoxFunction";
+            this.textBoxFunction.Size = new System.Drawing.Size(392, 22);
+            this.textBoxFunction.TabIndex = 1;
+            // 
+            // buttonPlot
+            // 
+            this.buttonPlot.Location = new System.Drawing.Point(232, 359);
+            this.buttonPlot.Name = "buttonPlot";
+            this.buttonPlot.Size = new System.Drawing.Size(95, 23);
+            this.buttonPlot.TabIndex = 2;
+            this.buttonPlot.Text = "Построить";
+            this.buttonPlot.UseVisualStyleBackColor = true;
+            this.buttonPlot.Click += new System.EventHandler(this.buttonPlot_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(413, 358);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(107, 23);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonPlot);
+            this.Controls.Add(this.textBoxFunction);
+            this.Controls.Add(this.pictureBoxGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxGraph;
+        private System.Windows.Forms.TextBox textBoxFunction;
+        private System.Windows.Forms.Button buttonPlot;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ MathFunction/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NCalc;
+
+namespace MathFunction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPlot_Click(object sender, EventArgs e)
+        {
+            // Получаем функцию от пользователя
+            string function = textBoxFunction.Text;
+
+            // Создаем битмап и объект Graphics
+            Bitmap bitmap = new Bitmap(pictureBoxGraph.Width, pictureBoxGraph.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            // Задаем систему координат и масштабирование
+            double minX = -10;
+            double maxX = 10;
+            double minY = -10;
+            double maxY = 10;
+
+            double scaleX = bitmap.Width / (maxX - minX);
+            double scaleY = bitmap.Height / (maxY - minY);
+
+            // Создаем перо для рисования (можете выбрать другой цвет)
+            Pen pen = new Pen(Color.Black);
+
+            double step = 0.01; // Шаг для построения точек
+
+            for (double x = minX; x <= maxX; x += step)
+            {
+                // Здесь вычисляем y в зависимости от x и функции пользователя
+                double y = CalculateY(x, function);
+
+                // Переводим координаты и рисуем точку
+                int xPix = (int)((x - minX) * scaleX);
+                int yPix = (int)((maxY - y) * scaleY);
+                graphics.DrawRectangle(pen, xPix, yPix, 1, 1);
+            }
+
+            // Отображаем график на PictureBox
+            pictureBoxGraph.Image = bitmap;
+        }
+
+        private double CalculateY(double x, string function)
+        {
+            // Здесь используем библиотеку NCalc для вычисления значения функции
+            NCalc.Expression expression = new NCalc.Expression(function);
+            expression.Parameters["x"] = x;
+            double y = (double)expression.Evaluate();
+            return y;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // Очистить текстовое поле с функцией
+            textBoxFunction.Clear();
+
+            // Очистить PictureBox с графиком
+            pictureBoxGraph.Image = null;
+        }
+    }
+}
